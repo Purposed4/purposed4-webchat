267d9148b46e98f315709de34adcd23b2e16ceab
Purposed4 <bernardhart@purposed4.com>
2026-03-22 01:19:28 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -451,5 +451,4 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(PORT, () => {
-  console.log(\`Purposed4 web chat running at http://localhost:\${PORT}\`);
-});
+  console.log(`Purposed4 web chat running at http://localhost:${PORT}`);
